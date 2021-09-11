63d5c7f1268c5b61ac5633bd485e55d9ce54ee1c
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-09-11 01:52:01 -0700

    completed isTreeSymmetric

---
+++ javascript/is-tree-symmetric.js
@@ -0,0 +1,54 @@
+//
+// Binary trees are already defined with this interface:
+// function Tree(x) {
+//   this.value = x;
+//   this.left = null;
+//   this.right = null;
+// }
+function isTreeSymmetric(t) {
+  if (!t) return true;
+
+  return isSymmetric(t, t);
+
+}
+
+function isSymmetric(nodeA, nodeB) {
+  let outerGood = false;
+  let innerGood = false;
+  if (nodeA.left === null && nodeB.right === null) {
+    outerGood = true;
+  } else if (!nodeA.left || !nodeB.right) {
+    return false;
+  } else if (nodeA.left.value !== nodeB.right.value) {
+    return false;
+  } else {
+    outerGood = isSymmetric(nodeA.left, nodeB.right);
+  }
+
+  if (nodeA.right === null && nodeB.left === null) {
+    innerGood = true;
+  } else if (!nodeA.right || !nodeB.left) {
+    return false;
+  } else if (nodeA.right.value !== nodeB.left.value) {
+    return false;
+  } else {
+    innerGood = isSymmetric(nodeA.right, nodeB.left);
+  }
+
+  return outerGood && innerGood;
+}
+
+/*
+start with a left and right pointer respectively
+if outer children do not match,
+    return false
+else
+    if they do not terminate
+        return if they are symmetrical or not
+if inner children do not match,
+    return false
+else
+    if they do not terminate
+        return if they are symmetrical or not
+
+*/
