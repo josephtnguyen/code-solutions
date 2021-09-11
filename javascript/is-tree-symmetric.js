e1f06308d5fde697fe66bbd154d797a5c21c24a3
Joseph Nguyen <josephtringuyen2@gmail.com>
2021-09-11 10:00:47 -0700

    revised isTreeSymmetric

--- javascript/is-tree-symmetric.js
+++ javascript/is-tree-symmetric.js
@@ -6,37 +6,56 @@
 //   this.right = null;
 // }
 function isTreeSymmetric(t) {
-  if (!t) return true;
+  // if (!t) return true;
 
   return isSymmetric(t, t);
 
 }
 
 function isSymmetric(nodeA, nodeB) {
-  let outerGood = false;
-  let innerGood = false;
-  if (nodeA.left === null && nodeB.right === null) {
-    outerGood = true;
-  } else if (!nodeA.left || !nodeB.right) {
-    return false;
-  } else if (nodeA.left.value !== nodeB.right.value) {
-    return false;
-  } else {
-    outerGood = isSymmetric(nodeA.left, nodeB.right);
-  }
+  if (nodeA === null && nodeB === null) return true;
+  if (nodeA === null || nodeB === null || nodeA.value !== nodeB.value) return false;
 
-  if (nodeA.right === null && nodeB.left === null) {
-    innerGood = true;
-  } else if (!nodeA.right || !nodeB.left) {
-    return false;
-  } else if (nodeA.right.value !== nodeB.left.value) {
-    return false;
-  } else {
-    innerGood = isSymmetric(nodeA.right, nodeB.left);
-  }
+  return isSymmetric(nodeA.left, nodeB.right) && isSymmetric(nodeA.right, nodeB.left);
 
-  return outerGood && innerGood;
+  // let outerGood = false;
+  // let innerGood = false;
+  // if (nodeA.left === null && nodeB.right === null) {
+  //     outerGood = true;
+  // } else if (!nodeA.left || !nodeB.right) {
+  //     return false;
+  // } else if (nodeA.left.value !== nodeB.right.value) {
+  //     return false;
+  // } else {
+  //     outerGood = isSymmetric(nodeA.left, nodeB.right);
+  // }
+
+  // if (nodeA.right === null && nodeB.left === null) {
+  //     innerGood = true;
+  // } else if (!nodeA.right || !nodeB.left) {
+  //     return false;
+  // } else if (nodeA.right.value !== nodeB.left.value) {
+  //     return false;
+  // } else {
+  //     innerGood = isSymmetric(nodeA.right, nodeB.left);
+  // }
+
+  // return outerGood && innerGood;
 }
+
+/*
+start on left and right
+check if left and right are the same
+  if both terminate,
+    return true
+  if only one terminates
+    return false
+  if their values don't match
+    return false
+  else we know left and right are the same and they have children
+    check if outers and inners are the same
+
+*/
 
 /*
 start with a left and right pointer respectively
